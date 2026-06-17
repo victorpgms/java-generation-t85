a90784c0def81cc46a2a8cd3f5e0e35425a87202
Victor Pedro <victorr.pgms@gmail.com>
2026-06-17 15:24:24 -0300

    aula 06 array list

---
+++ aula_05_array2/src/aula_05_array2/Exe02.java
@@ -0,0 +1,24 @@
+package aula_05_array2;
+
+import java.util.Scanner;
+
+public class Exe02 {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int vetorTamanho = 10;
+		int vetor [] = new int [vetorTamanho];
+		int soma = 0;
+		int media = 0;
+		
+		for (int i = 0; i < vetor.length; i++) {
+			
+		}
+		
+		
+
+	}
+
+}
---
+++ aula_06_collections/src/aula_06_collections/CollectionArreyList.java
@@ -0,0 +1,35 @@
+package aula_06_collections;
+
+import java.util.ArrayList;
+
+public class CollectionArreyList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		
+		numeros.add(2);
+		numeros.add(5);
+		numeros.add(1);
+		numeros.add(6);
+		numeros.add(1);
+		
+		
+		for (int numero : numeros) {
+			System.out.println(numero);
+		}
+		
+		System.out.println("O objeto que está presente no índice 3: " + numeros.get(3));
+		System.out.println("O objeto que está presente no índice 1: " + numeros.indexOf(1));
+		
+		numeros.set(3, 25);
+		
+		System.out.println("O objeto que está presente no índice 3: " + numeros.get(3));
+		System.out.println("O objeto que está presente no índice : " + numeros.indexOf(1));
+		
+		numeros.remove(3);
+		
+		System.out.println("O objeto que está presente no índice 3: " + numeros.get(3));
+	}
+
+}
